c5ddd52866dc035a3a5d7fa7fed9eaa43f79b978
Prateek Goyal <prateek.goyal5@gmail.com>
2018-11-11 08:42:25 +0530

    feat: Adding Player class

--- js/app.js
+++ js/app.js
@@ -24,12 +24,25 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
 
+}
+
+// Update method for player
+Player.prototype.update = function() {
+
+}
+
+// Update method for player
+Player.prototype.render = function() {
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
